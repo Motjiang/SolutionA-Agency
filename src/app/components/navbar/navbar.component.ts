807764273e032895e75afad98bff2fa0a85df6a7
Motjiang <motjiangbrilliant@gmail.com>
2025-06-20 12:28:14 +0200

    Added resposive navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [FooterComponent, RouterOutlet, RouterLink],
   template: `
-    <p>
-      navbar works!
-    </p>
+    <nav>
+      <div class="nav__header">
+        <div class="nav__logo">
+          <a href="#">Solution<span>A</span></a>
+        </div>
+        <div class="nav__menu__btn" id="menu-btn">
+          <i class="ri-menu-line"></i>
+        </div>
+      </div>
+      <ul class="nav__links" id="nav-links">
+        <li><a [routerLink] = "['/home']" >Home</a></li>
+        <li><a [routerLink] = "['/home/about-us']" >About Us</a></li>
+        <li><a [routerLink] = "['/home/feedback']" >Client</a></li>
+        <li><a [routerLink] = "['/home/reach-us']" >Contact</a></li>
+        <li><a [routerLink] = "['/home/portfolio']" >Portfolio</a></li>
+      </ul>
+      <div class="nav__btns">
+        <button class="btn">Let's Talk</button>
+      </div>
+    </nav>
 
     <router-outlet/>
 
